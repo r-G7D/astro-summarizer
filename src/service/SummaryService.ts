baccdab8b1857e3869ff6d66d8c0d4f86fe2f34c
r_g7d <danangr123@gmail.com>
2023-06-26 21:21:08 +0700

    Update SummaryService.ts

--- src/service/SummaryService.ts
+++ src/service/SummaryService.ts
@@ -1,15 +1,12 @@
-import { token } from "../../astro.config.mjs";
-
 interface SummaryResponse {
   summary_text: string
 }
 
 export async function getSummary(text: string): Promise<string> {
-  // const token = import.meta.env.PUBLIC_API_TOKEN;
     const response = await fetch(
         "https://api-inference.huggingface.co/models/facebook/bart-large-cnn",
         {
-            headers: { Authorization: `Bearer ${token}` },
+            headers: { Authorization: `Bearer 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` },
             method: "POST",
             body: JSON.stringify(text),
         }
