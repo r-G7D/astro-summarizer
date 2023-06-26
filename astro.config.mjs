3ceb6d5ba3ee728eac849e4c0f650f5905a391f9
r_g7d <danangr123@gmail.com>
2023-06-26 21:19:30 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import solidJs from "@astrojs/solid-js";
 
+export const token = process.env.PUBLIC_API_TOKEN;
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://r-G7D.github.io",
